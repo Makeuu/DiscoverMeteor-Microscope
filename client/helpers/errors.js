2ade556ca64949fca75860c02a2fa71a51b364ae
Matthieu Mercier <mattdu77@live.fr>
2016-10-21 11:04:31 +0200

    Effacer les erreurs après 3 secondes.

--- client/helpers/errors.js
+++ client/helpers/errors.js
@@ -4,3 +4,10 @@ Errors = new Meteor.Collection(null);
 throwError = function(message) {
   Errors.insert({message: message});
 };
+
+Template.error.onRendered(function() {
+  var error = this.data;
+  Meteor.setTimeout(function () {
+    Errors.remove(error._id);
+  }, 3000);
+});
