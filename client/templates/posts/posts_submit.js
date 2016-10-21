37c5551d0b89f967c83271d376e8dfd8e23e5bf9
Matthieu Mercier <mattdu77@live.fr>
2016-10-21 09:30:57 +0200

    S'assurer de l'unicité de l'url du post

--- client/templates/posts/posts_submit.js
+++ client/templates/posts/posts_submit.js
@@ -9,8 +9,13 @@ Template.postSubmit.events({
 
     Meteor.call('postInsert', post, function(error, result) {
       // affiche l'erreur à l'utilisateur et s'interrompt
-      if (error)
-      return alert(error.reason);
+      if (error){
+        return alert(error.reason);
+      }
+      // affiche ce résultat mais route tout de même
+      if (result.postExists){
+        alert('Ce lien a déjà été utilisé');
+      }
       Router.go('postPage', {_id: result._id});
     });
   }
