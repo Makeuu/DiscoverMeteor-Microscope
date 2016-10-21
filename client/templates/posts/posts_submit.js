c2b382e87f02314cd1a009e169c11f1902decde2
Matthieu Mercier <mattdu77@live.fr>
2016-10-21 11:01:15 +0200

    Utilisation du rapport d'erreurs

--- client/templates/posts/posts_submit.js
+++ client/templates/posts/posts_submit.js
@@ -10,11 +10,11 @@ Template.postSubmit.events({
     Meteor.call('postInsert', post, function(error, result) {
       // affiche l'erreur à l'utilisateur et s'interrompt
       if (error){
-        return alert(error.reason);
+        return throwError(error.reason);
       }
       // affiche ce résultat mais route tout de même
       if (result.postExists){
-        alert('Ce lien a déjà été utilisé');
+        throwError('This link has already been posted');
       }
       Router.go('postPage', {_id: result._id});
     });
