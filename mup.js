1e764c125d224c968e1b7ea0ae0becebce247812
Matthieu Mercier <mattdu77@live.fr>
2016-11-14 18:07:55 +0100

    Essai d'un deploiement

---
+++ mup.js
@@ -0,0 +1,45 @@
+module.exports = {
+  servers: {
+    one: {
+      host: '82.223.71.158',
+      username: 'root',
+      // pem:
+      password: 'Matt6288merc'
+      // or leave blank for authenticate from ssh-agent
+    }
+  },
+
+  meteor: {
+    name: 'microscope',
+    path: '.',
+    servers: {
+      one: {}
+    },
+    buildOptions: {
+      serverOnly: true,
+    },
+
+    env: {
+      ROOT_URL: 'http://mercierm.fr',
+      PORT: 80,
+      MONGO_URL: 'mongodb://localhost/meteor'
+    },
+
+    dockerImage: 'abernix/meteord:base',
+    deployCheckWaitTime: 30
+  },
+
+  ssl: {
+    port: 443,
+    crt: 'www.mercierm.fr_ssl_certificate.cer',
+    key: 'mercierm.fr_private_key.key',
+  },
+
+  mongo: {
+    oplog: true,
+    port: 27017,
+    servers: {
+      one: {},
+    },
+  },
+};
